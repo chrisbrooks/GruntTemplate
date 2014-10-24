97d95c95cf845e9c9e737aa4f3108b96cebeb627
chrisbrooks <chris.brooks@orange.com>
2014-10-25 00:56:05 +0100

    adding initial files

---
+++ GruntFile.js
@@ -0,0 +1,271 @@
+var scriptsList = {
+	'app/build/scripts/combined.min.js': [
+		'app/src/assets/scripts/main.js'
+	]
+};
+
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		jshint: {
+
+			files: 'app/src/assets/scripts/*.js',
+
+			options: {
+				globals: {
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		},
+
+		uglify: {
+
+			development : {
+				options: {
+					beautify: true,
+					compress: false
+				},
+				files: scriptsList
+			},
+
+			release : {
+				files: scriptsList
+			}
+		},
+
+		sass: {
+
+			development: {
+				options: {
+				},
+				files: {
+					'app/build/styles/style.css': 'app/src/assets/styles/style.scss',
+				}
+			},
+
+			release: {
+				options: {
+					outputStyle: 'compressed'
+				},
+				files: {
+					'app/build/styles/style.css': 'app/src/assets/styles/style.scss',
+				}
+			}
+		},
+
+		autoprefixer: {
+
+			build: {
+				expand: true,
+				flatten: true,
+				src: 'app/build/styles/**/*.css',
+				dest: 'app/build/styles/'
+			}
+		},
+
+		copy: {
+
+			images: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/images',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'app/build/images/'
+				}]
+			},
+
+			media: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/media',
+					src: ['**/*.{png,jpg,gif,svg}'],
+					dest: 'app/build/media/'
+				}]
+			},
+
+			fonts: {
+				files: [
+					{
+						expand: true,
+						cwd: 'app/src/assets/fonts',
+						src: ['**/*.{eot,svg,ttf,woff}'],
+						dest: 'app/build/fonts/'
+					}
+				]
+			},
+
+			videos: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets/video',
+					src: ['**/*'],
+					dest: 'app/build/video/'
+				}]
+			},
+
+			pdf: {
+				files: [{
+					expand: true,
+					cwd: 'app/src/assets',
+					src: ['**/*.pdf'],
+					dest: 'app/build/'
+				}]
+			}
+		},
+
+		clean: {
+
+			build: {
+				src: ['app/build/']
+			}
+		},
+
+		connect: {
+
+			server: {
+				options: {
+					base: 'app/build/',
+					hostname: '127.0.0.1',
+					keepalive: true,
+					open: {
+						target: 'http://localhost:8020'
+					},
+					port: 8020
+				}
+			}
+		},
+
+		assemble: {
+
+			options: {
+				prettify: {
+					indent: 4
+				},
+				marked: {
+					sanitize: false
+				},
+				production: true,
+				layoutdir: 'src/layouts',
+				partials: ['src/partials/**/*.hbs']
+			},
+
+			development: {
+				options: {
+					data: 'config/development/*.json'
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'src/pages',
+						src: ['**/*.hbs'],
+						dest: 'build/'
+					}
+				]
+			},
+
+			release: {
+				options: {
+					data: 'config/release/*.json'
+				},
+				files: [
+					{
+						expand: true,
+						cwd: 'src/pages',
+						src: ['**/*.hbs'],
+						dest: 'build/'
+					}
+				]
+			},
+		},
+
+		watch: {
+
+			// Run SASS compliler when precompiled files are changed
+			styles: {
+				files: 'app/src/assets/styles/**/*.scss',
+				tasks: ['sass:development'],
+				options: {
+					livereload: false,
+					spawn: false
+				}
+			},
+
+			images: {
+				files: ['app/src/assets/images/**/*.{png,jpg,gif}'],
+				tasks: ['copy:images'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			media: {
+				files: ['app/src/assets/media/**/*.{png,jpg,gif}'],
+				tasks: ['copy:media'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			videos: {
+				files: ['app/src/assets/video/**/*.{mp4,ogv,webm}'],
+				tasks: ['copy:videos'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+			scripts: {
+				files: ['app/src/assets/scripts/**/*.js'],
+				tasks: ['uglify:development'],
+				options: {
+					livereload: false,
+					interrupt: true
+				}
+			},
+
+		},
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('assemble');
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+	grunt.loadNpmTasks('grunt-html-validation');
+
+	// Register task(s) to run when 'grunt' command is executed
+	grunt.registerTask('default', ['development']);
+
+	// Register task(s) to run when 'grunt development' command is executed
+	grunt.registerTask('development', [
+		'jshint',
+		'clean',
+		'sass:development',
+		'autoprefixer',
+		'copy',
+		'uglify:development',
+		'watch'
+	]);
+
+		// Register task(s) to run when 'grunt release' command is executed
+	grunt.registerTask('release', [
+		'clean',
+		'sass:release',
+		'autoprefixer',
+		'copy',
+		'uglify:release',
+		'assemble:release'
+	]);
+
+};
